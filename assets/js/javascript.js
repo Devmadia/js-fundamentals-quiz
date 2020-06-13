259b9fec45a089c7e856f91664d42143509b7385
Devmadia <agathaphan@icloud.com>
2020-06-13 18:22:45 -0500

    outlined variables and functions necessary for program

---
+++ assets/js/javascript.js
@@ -0,0 +1,21 @@
+// variable for high scores to be stored to
+// variable for timer
+// variable for questions
+// function to call high scores
+// function for user initials for high score association and localStorage
+// function for null answer with user initials before continuing
+// function to store high scores to localStorage
+// function to clear high scores
+
+// function for timer countdown
+
+// function to startQuiz
+
+// array of questions
+// function to score user for correct answers
+// function to call questions
+// function to introduce next question
+
+
+// function to penalize timer by -10 seconds for incorrect answers
+
