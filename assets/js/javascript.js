28bff3a9a2b9faee0cdac20b505085d0dc909914
Devmadia <agathaphan@icloud.com>
2020-06-13 19:22:16 -0500

    added questionbank

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -12,6 +12,54 @@
 // function to startQuiz
 
 // array of questions
+var questions = [
+    { 
+        question: "What are variables used for in JavaScript Programs?",
+        choice1: "Storing numbers, dates, or other values",
+        choice2: "Varying randomly",
+        choice3: "Causing high-school algebra flashbacks",
+        choice4: "None of the above",
+        answer: 1
+    },
+
+    { 
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "<js>",
+        choice2: "<scripting>",
+        choice3: "<script>",
+        choice4: "<javascript>",
+        answer: 3
+    },
+
+    { 
+        question: "A very useful tool used during development and debugging for printing content to the debugger is",
+        choice1: "Javascript",
+        choice2: "Terminal/Bash",
+        choice3: "For Loops",
+        choice4: "Console.log",
+        answer: 4
+    },
+
+    { 
+        question: "The condition in an if/else statement is enclosed within",
+        choice1: "Quotes",
+        choice2: "Curly Brackets",
+        choice3: "Parenthesis",
+        choice4: "Square Brackets",
+        answer: 2
+    },
+
+    { 
+        question: "Commonly used data types DO NOT include:",
+        choice1: "Strings",
+        choice2: "Booleans",
+        choice3: "Alerts",
+        choice4: "Numbers",
+        answer: 4
+    },
+]
+
+
 // function to score user for correct answers
 // function to call questions
 // function to introduce next question
